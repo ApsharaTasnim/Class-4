c4167cd0fafaf4d4d8d5e234d2c086ad47479f2e
ApsharaTasnim <alferdstanley@gmail.com>
2024-10-19 22:32:56 +0600

    add4

---
+++ main.py
@@ -0,0 +1,13 @@
+num=23
+print("Table of 23")
+for i in range(1,11):
+    mul=num*i
+    print("23 x %d=%d" %(i,mul))
+
+    
+num=29
+print("Table of 29")
+for i in range(1,11):
+    mul=num*i
+    print("29 x %d=%d" %(i,mul))
+    
---
+++ sum.py
@@ -0,0 +1,6 @@
+num=1
+sum=0
+while(num<=32):
+       sum = sum+num
+       num = num+1
+print("The sum of the first 32 numbers is :",sum)
